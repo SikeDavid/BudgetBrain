4a99e6441a8cf2f96406499ab740e0f4697e2a44
Jediman <sike.david@gmail.com>
2026-07-08 15:30:10 +0200

    authmiddleware error message

--- Web/Backend/middleware/authMiddleware.js
+++ Web/Backend/middleware/authMiddleware.js
@@ -1,28 +1,28 @@
 import jwt from 'jsonwebtoken';
 
 function authMiddleware(req, res, next) {
-    // const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    // if (!authHeader) return res.status(401).json({message: "Missing authorization header"});
+    if (!authHeader) return res.status(401).json({message: "Missing authorization header"});
 
-    // const [type, token] = authHeader.split(" ");
+    const [type, token] = authHeader.split(" ");
 
-    // if (type != "Bearer" || !token) return res.status(401).json({message: "Invalid authorization format"});
+    if (type != "Bearer" || !token) return res.status(401).json({message: "Invalid authorization format"});
 
-    // try {
-    //     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    //     req.user = decoded.userId;
-    //     req.user = {
-    //         id: decoded.userId,
-    //         username: decoded.username
-    //     };
-    //     next();
-    // } catch (err) {
-    //     console.error("Server error", err);
-    //     return res.status(500).json({message: "Invalid or expired token"});
-    // }
-    req.user = { id: 2}
+        req.user = decoded.userId;
+        req.user = {
+            id: decoded.userId,
+            username: decoded.username
+        };
+        next();
+    } catch (err) {
+        console.error("Server error", err);
+        return res.status(500).json({message: err.message});
+    }
+    // req.user = { id: 2}
     if (!req.user.id) return res.status(401).json({message: "You are not loged in"});
     next();
 }
