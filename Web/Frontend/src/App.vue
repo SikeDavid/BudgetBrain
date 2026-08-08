6cdf668c2246cc4d70763aad9be92888aae5c8c7
BitMiller <gp.bitmiller@gmail.com>
2026-08-08 19:44:00 +0200

    @260808-6-1943

--- Web/Frontend/src/App.vue
+++ Web/Frontend/src/App.vue
@@ -1,11 +1,36 @@
-<script setup></script>
+<script setup>
+    import { ref } from "vue";
+    const userName = ref("Base");
+    const password = ref("Pwd");
+
+    function login() {
+        //alert("Username: " + userName.value + "\nPassword: " + password.value);
+        alert(`Username: ${userName.value}\nPassword: ${password.value}`);
+    }
+
+    function isBothFilled() {
+        if ()
+    }
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <h1>BudgetBrain</h1>
+    <label>Username:</label>
+    <br>
+    <input type="text" v-model="userName">
+    <br>
+    <label>Password:</label>
+    <br>
+    <input type="password" v-model="password">
+    <br>
+    <p>You typed: {{ userName }}, {{ password }}</p>
+    <br>
+    <button @click="login">Login</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+    h1 {
+        color: grey;
+        background-color: black;
+    }
+</style>
